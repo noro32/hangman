493f1475fca36ef5ce7bffcbacb7140cc68dd6e5
Mehak Shafiq <mehakshafiq90@gmail.com>
2026-04-10 23:00:01 -0400

    clean up code

--- hangman.js
+++ hangman.js
@@ -27,10 +27,6 @@ updatePage();
 let clueString = "";
 for(let i = 0; i< word.length; i++)
 
-
-
-
- 
 {
     var currentLetter = word.charAt(i);
     if(guesses.indexOf(currentLetter)>=0){
@@ -53,35 +49,49 @@ image.src = `images/hangman${guessCount}.gif`;
 
  }
  let guessLetter = function(){
-    //  dont allow guesses before a word is chosen
-    if(word ===""){
-     alert("press new game to start");
-     return;
-    }
- // do not allow guesses when the game is finished 
-if(guessCount <= 0){
-    alert("The game is over! Press New Game to play again.");
-    return;
-}
-if(guessArea.textContent.startsWith("You")){
-    alert("The game is over! Press New Game to play again.");
+
+//  Don't allow guesses before a word is chosen
+if(word === ""){
+    alert("Please press New Game to start!");
     return;
 }
 
+//  Don't allow guesses when the game is finished
+let guessArea = document.getElementById("guesses");
+if(guessCount <= 0 || guessArea.textContent.startsWith("You")){
+    alert("The game is over! Press New Game to play again.");
+    return;
+}
 
  let input = document.getElementById("guess");
  let letter = input.value;
  letter = letter.toLowerCase();
- //Don't allow the same letter to be guessed twice
- if(guesses.indexOf(letter) >= 0){
+
+//  Don't allow the same letter to be guessed twice
+if(guesses.indexOf(letter) >= 0){
     alert("You already guessed '" + letter + "'! Try a different letter.");
     input.value = "";
     return;
 }
+
  if(word.indexOf(letter)<0){
     guessCount--;
  }
  guesses+=letter;
  input.value="";
  updatePage();
+
+//  Dialog for winning and losing 
+let clueString = "";
+for(let i = 0; i < word.length; i++){
+    if(guesses.indexOf(word.charAt(i)) >= 0){
+        clueString += word.charAt(i);
+    }
+}
+if(clueString === word){
+    guessArea.textContent = "You WIN! The word was: " + word;
+} else if(guessCount <= 0){
+    guessArea.textContent = "You LOSE! The word was: " + word;
+}
+
  }
